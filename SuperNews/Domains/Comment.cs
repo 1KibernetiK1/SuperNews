05dc82e7902df6bf49cd683c86d54f2e7fb4e75c
1KibernetiK1 <123451040o@gmail.com>
2023-02-06 21:06:39 +0500

    Добавьте файлы проекта.

---
+++ SuperNews/Domains/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SuperNews.Domains
+{
+    [Table("Comments")]
+    public class Comment
+    {
+        [Key]
+        public long? CommentId { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual ICollection<News> ArticlesOfComments { get; set; }
+    }
+}
